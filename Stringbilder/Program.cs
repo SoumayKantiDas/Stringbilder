401f3307896dbc652f2569992cd7e175021a2968
Soumay kanti Das <anikds2859@outlook.com>
2021-06-07 15:53:22 +0600

    Add project files.

---
+++ Stringbilder/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+public class Program
+{
+	public static void Main()
+	{
+		StringBuilder sb = new StringBuilder("Hello ", 50);
+
+		sb.Append("World!!");
+		sb.AppendLine("Hello C#!");
+		sb.AppendLine("This is new line.");
+
+		Console.WriteLine(sb);
+	}
+}
